0576bd238e6c749be23e1962697afe40ee391410
droruri <dror.uri@equalum.io>
2022-12-19 21:14:27 +0200

    design app component to contain all objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/Header";
+import {ContactForm} from "./components/ContactForm";
+import {ContactDemo} from "./components/ContactDemo";
+import {CompanyLeaders} from "./components/CompanyLeaders";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="contact-us-page">
+            <Header/>
+            <div className="contact-us-actions">
+                <ContactDemo/>
+                <ContactForm/>
+            </div>
+            <CompanyLeaders/>
+        </div>
+    );
 }
 
 export default App;
